0cf40ee3e025ca70a421edbbae0b6b9f2ae99e50
Simon Kirchhoff <Simon@DESKTOP-INI1914.hgst.hgi.rub.de>
2019-08-29 14:25:58 +0200

    edit company

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CompanyService } from './company.service';
 import { Company } from '../shared/company.model';
 import { CompanyAdress } from '../shared/company.adress.model';
@@ -6,6 +6,7 @@ import { BillingAdress } from '../shared/billing.adress.model';
 import { Person } from '../shared/person.model';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-company',
@@ -13,6 +14,7 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./company.component.scss'],
 })
 export class CompanyComponent implements OnInit {
+  @ViewChild('tabs') tabs : IonTabs;
   company: Company;
   companyAdress: CompanyAdress;
   billingAdress: BillingAdress;

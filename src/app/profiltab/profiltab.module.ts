a1639b7723e1ba2fd483e852f81bdc69b5f12fc3
n12no <theoldoneno@gmail.com>
2019-09-03 16:47:26 +0200

    fixed

--- src/app/profiltab/profiltab.module.ts
+++ src/app/profiltab/profiltab.module.ts
@@ -7,14 +7,7 @@ import { ProfilTabRoutingModule } from './profiltab.router.module';
 import { GeneralComponent } from './general/general.component';
 import { ContactComponent } from './contact/contact.component';
 import { BillingAdressComponent } from './billing-adress/billing-adress.component';
-<<<<<<< HEAD
 import { FirestoreContactService } from '../services/firestore-contact.service';
-=======
-import { CompanyService } from '../company/company.service';
-import { GeneralEditComponent } from './general/general-edit/general-edit.component';
-import { ContactEditComponent } from './contact/contact-edit/contact-edit.component';
-import { BillingAdressEditComponent } from './billing-adress/billing-adress-edit/billing-adress-edit.component';
->>>>>>> 0b715c7b63d3f8a22d3424b76a8f5974131a0f9d
 
 
 @NgModule({
@@ -36,11 +29,7 @@ import { BillingAdressEditComponent } from './billing-adress/billing-adress-edit
     ProfilTabRoutingModule
   ],
   providers: [
-<<<<<<< HEAD
     FirestoreContactService
-=======
-  
->>>>>>> 0b715c7b63d3f8a22d3424b76a8f5974131a0f9d
   ]
 })
 export class ProfiltabModule { }

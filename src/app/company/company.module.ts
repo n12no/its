32080a5f1f65a471e3535130e01a13521af7d8a4
Simon Kirchhoff <Simon@DESKTOP-INI1914.hgst.hgi.rub.de>
2019-09-03 12:54:42 +0200

    delete

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -4,20 +4,12 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { CompanyComponent } from './company.component';
 import { CompanyService } from './company.service';
-import { ContactEditComponent } from './edit/contact-edit/contact-edit.component';
-import { CompanyEditComponent } from './edit/company-edit/company-edit.component';
-import { CompanyAdressEditComponent } from './edit/company-adress-edit/company-adress-edit.component';
-import { BillingAdressEditComponent } from './edit/billing-adress-edit/billing-adress-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 
 @NgModule({
   declarations: [
-    CompanyComponent,
-    ContactEditComponent,
-    CompanyEditComponent,
-    CompanyAdressEditComponent,
-    BillingAdressEditComponent
+    CompanyComponent
   ],
   imports: [
     CommonModule,
@@ -28,22 +20,6 @@ import { AuthenticationService } from '../services/authentication.service';
     {
       path: '',
       component: CompanyComponent
-    },
-    {
-      path: 'contact-edit',
-      component: ContactEditComponent
-    },
-    {
-      path: 'company-edit',
-      component: CompanyEditComponent
-    },
-    {
-      path: 'company-adress-edit',
-      component: CompanyAdressEditComponent
-    },
-    {
-      path: 'billing-adress-edit',
-      component: BillingAdressEditComponent
     }
   ])
   ],
